0d781d8e3c93bb6dab52e44d93875ee794849b96
LesVu <johndie005@gmail.com>
2024-01-19 13:38:35 +0800

    Up

--- files/start.sh
+++ files/start.sh
@@ -4,6 +4,10 @@ export WLR_RENDER_DRM_DEVICE=/dev/dri/card0
 export XDG_RUNTIME_DIR=/run/user/1000
 export WLR_LIBINPUT_NO_DEVICES=1
 export WLR_BACKENDS=headless
+# export WAYLAND_DISPLAY=wayland-1
 
-wayfire &
-WAYLAND_DISPLAY=wayland-1 wayvnc 0.0.0.0 &
+sudo chown root:video /dev/dri/renderD128
+sudo mkdir -p $XDG_RUNTIME_DIR
+sudo chown $(id -nu):$(id -ng) $XDG_RUNTIME_DIR
+wayfire
+# wayvnc 0.0.0.0

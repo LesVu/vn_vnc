eafbf161c62399f786e84e2670ae709a4f00f91b
LesVu <johndie005@gmail.com>
2024-07-29 17:55:42 +0800

    up

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build . -f Dockerfile --no-cache --build-arg="user=abc" --tag lesvu/vn_vnc:latest
-docker push lesvu/vn_vnc:latest
+docker build . -f Dockerfile.boxed --no-cache --build-arg="user=abc" --tag lesvu/vn_vnc:latest
+docker push lesvu/vn_vnc:boxed
+docker build . -f Dockerfile.fex --no-cache --build-arg="user=abc" --tag lesvu/vn_vnc:latest
+docker push lesvu/vn_vnc:fex
